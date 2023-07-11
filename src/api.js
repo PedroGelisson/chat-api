793a9dea483ba24af9f3740b5e5aa3cb0719cadc
ArthurFSchmidt <arthur-fschmidt@educar.rs.gov.br>
2023-07-11 20:51:25 -0300

    reparos2

--- src/api.js
+++ src/api.js
@@ -1,12 +1,25 @@
- var express = require("express");
- var app = express();
+ const express = require("express");
+ const app = express();
  const salaController = require('./controllers/salaController');
  const token = require('./util/token');
  app.use(express.urlencoded({extended : true}));
  app.use(express.json());
 
  const router = express.Router();
+ 
+ app.use((req, res, next) =>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if(req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        return res.status(200).send({})
+    }
+    next(); 
+}
+)
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
  app.use('/sobre',router.get('/sobre',(req,res,next)=>{
     res.status(200).send({
     "nome":"API - CHAT",
